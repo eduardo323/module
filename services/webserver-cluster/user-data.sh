fb40883edb273d38f864548c043386b212e688bc
Eduardo Sulit <eduardo323@mac.com>
2022-12-08 14:58:44 -0800

    init

---
+++ services/webserver-cluster/user-data.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+cat > index.html <<EOF
+<h1>Hello, Prod World! Yay!</h1>
+<p>Current DB address: ${db_address}</p>
+<p>Current DB port: ${db_port}</p>
+EOF
+
+nohup busybox httpd -f -p ${server_port} &
